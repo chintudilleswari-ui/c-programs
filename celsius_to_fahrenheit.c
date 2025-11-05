1960bd830df6c19240ad0d954353b2bea066da16
chintudilleswari-ui <chintudilleswari@gmail.com>
2025-11-05 15:56:02 +0530

    Add files via upload

---
+++ celsius_to_fahrenheit.c
@@ -0,0 +1,11 @@
+//celsius to fahrenheit
+#include<stdio.h>
+int main()
+{
+	float celsius,fahrenheit;
+	printf("Enter temperature in celsius:");
+	scanf("%f",&celsius);
+	fahrenheit=(1.8*celsius)+32;
+	printf("Temperature in fahrenheit:%.2f",fahrenheit);
+	return 0;
+}
